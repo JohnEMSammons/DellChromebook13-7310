0ebebe6a6b19cda1c01b469542aa988857a37211
John Sammons <john_sammons@hotmail.com>
2020-07-02 20:22:10 -0400

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -2,10 +2,6 @@
 localectl set-x11-keymap us chromebook
 sudo apt update
 sudo apt upgrade
-./ubuntu-mainline-kernel.sh -i 5.7.7 -do -p .
-sudo apt install -y ./*.deb
-sudo apt install grub-customizer
 sudo rm -f /usr/bin/kblight
 sudo ln -s $PWD/kblightdb /usr/bin/kblight
 ./importkeys.sh
-cp .xprofile ~/.
